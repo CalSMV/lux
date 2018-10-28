e2b18ed77f4f578d75183996636b43f7b3c7bf5f
Marcus Plutowski <achierius@berkeley.edu>
2018-10-28 12:30:11 -0700

    Flesh out the can_interface a tiny tiny bit

---
+++ include/can_interface.h
@@ -0,0 +1,11 @@
+#ifndef __CAN_INTERFACE_H
+#define __CAN_INTERFACE_H
+
+/* Tasks for this segment:
+ * 1. Read input from hardware ports into software (HAL)
+ * 2. Recieve CAN information (? CAN Library)
+ * 3. Decode CAN messages into useable signals (Figure out what we want our signals to look like)
+ */
+
+
+#endif//__CAN_INTERFACE_H
