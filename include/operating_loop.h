fa584fd959b17323c3f65beca03a13bd54d120a3
Marcus Plutowski <achierius@gmail.com>
2019-01-27 19:20:55 -0800

    Flesh out operating_loop

--- include/operating_loop.h
+++ include/operating_loop.h
@@ -1,6 +1,12 @@
 #ifndef __LUX_OPERATING_LOOP_H
 #define __LUX_OPERATING_LOOP_H
 
+#include <chrono>
+#include <cstdint>
 
+using DURATION_TYPE = std::chrono::duration<uint64_t, std::milli>;
+constexpr DURATION_TYPE LOOP_TIME(100000);
+
+bool operatingLoop(std::chrono::system_clock::time_point prevTime, std::chrono::system_clock::time_point nextTime);
 
 #endif//__LUX_OPERATING_LOOP_H
