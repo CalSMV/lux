975569da995c7ca77da8df540af3e0d8a47693b9
Marcus Plutowski <achierius@gmail.com>
2019-01-31 13:04:44 -0800

    Implement and update controller code

--- include/controller.h
+++ include/controller.h
@@ -8,8 +8,9 @@ int BMSFaultLevel(std::tuple<int, int> BMSStat);
 int TimeoutFaultLevel(std::chrono::system_clock::time_point reference);
 
 //Needs: position, BMSStat, brake, accelerator
+//Returns: Fault level, if any
 int controllerUpdate(std::chrono::system_clock::time_point prevTime, std::chrono::system_clock::time_point currTime);
 
-int getSetpoint();
+double getSetpoint();
 
 #endif//__LUX_CONTROLLER_H
--- src/controller.cc
+++ src/controller.cc
@@ -1,1 +1,73 @@
 #include "controller.h"
+
+#include <algorithm>
+#include <optional>
+
+#include "accelerator_reader.h"
+#include "brake_reader.h"
+#include "hall_reader.h"
+#include "observer.h"
+#include "lookup_table.h"
+#include "battery_reader.h"
+#include "constants.h"
+
+//TODO: Make optional?
+double setPoint_;
+double getSetpoint() {
+  return setPoint_;
+}
+
+int BMSFaultLevel(std::tuple<int, int> BMSStat) {
+  int temp = std::get<0>(BMSStat);
+  int volt = std::get<1>(BMSStat);
+
+  if(temp >= SHUTDOWN_TEMP || volt < MIN_VOLTAGE) {
+    return 2;
+  } else if (temp >= BREAK_TEMP) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+int TimeoutFaultLevel(std::chrono::system_clock::time_point reference) {
+  int timeout_fault = 0;
+  if(BRAKE_TIMEOUT != ZERO_TIME) {
+    if(std::chrono::duration_cast<DURATION_TYPE>(getBrakeTimestamp() - reference) >= BRAKE_TIMEOUT) {
+      timeout_fault = std::max(timeout_fault, BRAKE_FAULT_LEVEL);
+    }
+  }
+  if(ACCEL_TIMEOUT != ZERO_TIME) {
+    if(std::chrono::duration_cast<DURATION_TYPE>(getAccelTimestamp() - reference) >= ACCEL_TIMEOUT) {
+      timeout_fault = std::max(timeout_fault, ACCEL_FAULT_LEVEL);
+    }
+  }
+  if(BMS_TIMEOUT != ZERO_TIME) {
+    if(std::chrono::duration_cast<DURATION_TYPE>(getBMSTimestamp() - reference) >= BMS_TIMEOUT) {
+      timeout_fault = std::max(timeout_fault, BMS_FAULT_LEVEL);
+    }
+  }
+  if(HALL_TIMEOUT != ZERO_TIME) {
+    if(std::chrono::duration_cast<DURATION_TYPE>(getHallTimestamp() - reference) >= HALL_TIMEOUT) {
+      timeout_fault = std::max(timeout_fault, HALL_FAULT_LEVEL);
+    }
+  }
+}
+
+int controllerUpdate(std::chrono::system_clock::time_point prevTime, std::chrono::system_clock::time_point currTime) {
+  uint8_t accel = getAccelValue();
+  bool brake = getBrakeValue();
+  uint8_t position = getPosition();
+
+  int faultLevel = std::max(BMSFaultLevel(getBMSValues()), TimeoutFaultLevel(std::chrono::system_clock::now()));
+  if(faultLevel) {
+    setPoint_ = 0;
+    return faultLevel;
+  } else {
+    if(brake) {
+      setPoint_ = 0;
+    } else {
+      setPoint_ = accel * percent(getBMSThrottle());
+    }
+  }
+}
