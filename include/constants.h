9a0cc991ed90cd755ff991d4aae366bc7fccb984
Marcus Plutowski <achierius@gmail.com>
2019-01-31 12:50:02 -0800

    Add constants file

---
+++ include/constants.h
@@ -0,0 +1,36 @@
+#ifndef __LUX_CONSTANTS_H
+#define __LUX_CONSTANTS_H
+
+using DURATION_TYPE = std::chrono::duration<uint64_t, std::milli>;
+constexpr DURATION_TYPE ZERO_TIME(0);
+
+constexpr DURATION_TYPE LOOP_TIME(100000);
+
+constexpr DURATION_TYPE BRAKE_TIMEOUT(1000);
+constexpr int BRAKE_FAULT_LEVEL = 2;
+constexpr DURATION_TYPE ACCEL_TIMEOUT = ZERO_TIME;
+constexpr int ACCEL_FAULT_LEVEL = 1;
+constexpr DURATION_TYPE BMS_TIMEOUT(500);
+constexpr int BMS_FAULT_LEVEL = 2;
+constexpr DURATION_TYPE HALL_TIMEOUT(500);
+constexpr int HALL_FAULT_LEVEL = 2;
+
+constexpr int BREAK_TEMP = 0; //Placeholder
+constexpr int SHUTDOWN_TEMP = 0; //Placeholder
+constexpr int MIN_VOLTAGE = 0;
+
+enum PIN {
+};
+
+enum MOSFET {
+  SWITCH_A = 0,
+  SWITCH_B = 1,
+  SWITCH_C = 2,
+  PWM = 3,
+  ON = 1,
+  OFF = 0,
+  REVERSE = -1,
+};
+
+
+#endif//__LUX_CONSTANTS_H
