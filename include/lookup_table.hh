5575ee16c08877691bcd653f91435590dff77d6c
Marcus Plutowski <achierius@gmail.com>
2019-02-02 02:59:27 -0800

    Add lookup table

--- include/lookup_table.hh
+++ include/lookup_table.hh
@@ -1,6 +1,8 @@
 #ifndef __LUX_LOOKUP_TABLE_H
 #define __LUX_LOOKUP_TABLE_H
 
+static constexpr char gateData_[8] = {0, 49, 14, 52, 28, 13, 19, 0};
+
 std::tuple<int, int, int> lookupValue(uint8_t position, uint8_t setPoint);
 
 #endif//__LUX_LOOKUP_TABLE_H
