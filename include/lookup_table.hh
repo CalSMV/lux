d15656c46730eee15c5fc5fad310ea102a076583
Marcus Plutowski <achierius@gmail.com>
2019-02-02 15:35:49 -0800

    Implement lookup table

--- include/lookup_table.hh
+++ include/lookup_table.hh
@@ -1,8 +1,10 @@
 #ifndef __LUX_LOOKUP_TABLE_H
 #define __LUX_LOOKUP_TABLE_H
 
+#include <tuple>
+
 static constexpr char gateData_[8] = {0, 49, 14, 52, 28, 13, 19, 0};
 
-std::tuple<int, int, int> lookupValue(uint8_t position, uint8_t setPoint);
+std::tuple<int, int, int> lookupValue(std::tuple<bool, bool, bool> hallValues);
 
 #endif//__LUX_LOOKUP_TABLE_H
--- src/lookup_table.cc
+++ src/lookup_table.cc
@@ -1,1 +1,16 @@
 #include "lookup_table.hh"
+
+inline int getIntValue(const std::tuple<bool, bool, bool>& hallValues) {
+  return   (std::get<2>(hallValues) << 2)
+         + (std::get<1>(hallValues) << 1)
+         + (std::get<0>(hallValues) << 0);
+}
+
+std::tuple<int, int, int> lookupValue(std::tuple<bool, bool, bool> hallValues) {
+  unsigned char gateValues = gateData_[getIntValue(hallValues)];
+  return std::make_tuple<int, int, int>(
+    (((gateValues >> 0) & 0x01) - (((gateValues >> 1) & 0x01)) * 2),
+    (((gateValues >> 2) & 0x01) - (((gateValues >> 3) & 0x01)) * 2),
+    (((gateValues >> 4) & 0x01) - (((gateValues >> 5) & 0x01)) * 2)
+  );
+}
